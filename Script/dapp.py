6d53b111898708cb5eb4e431b2b73e99e2942dd0
Tegisty Hailay <tigisthay13@gmail.com>
2022-09-28 16:44:19 +0300

    web3 dapp

---
+++ Script/dapp.py
@@ -0,0 +1,9 @@
+with open('results.txt', 'w') as result:
+      result.write('certificate distrbution/Web3 dapp\n')
+      result.write('\n')     
+      result.write('NFT\n')
+      result.write('\n')
+      result.write(' Algorand\n')
+      result.write('\n')  
+
+       
