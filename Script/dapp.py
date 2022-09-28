c7092db21f6a350d47840e4b6817be45e0ff8059
Tegisty Hailay <tigisthay13@gmail.com>
2022-09-28 20:45:19 +0300

    certeficate distribution and smart contract

--- Script/dapp.py
+++ Script/dapp.py
@@ -1,9 +1,7 @@
 with open('results.txt', 'w') as result:
-      result.write('certificate distrbution/Web3 dapp\n')
+      result.write('Web3 dapp \n')
       result.write('\n')     
-      result.write('NFT\n')
+      result.write('nft\n')
       result.write('\n')
-      result.write(' Algorand\n')
-      result.write('\n')  
 
        
